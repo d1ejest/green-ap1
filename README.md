f788d361c5a91d532e62692dd1ec973e336dafd7
taa <andrey_tor@yahoo.com>
2023-12-06 18:29:20 +0600

    Readme

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# Technical Assignment using green-api
+
+This is an implementation of green-api work. The backend is implemented in python framework flask.
+
+
+
+
+
+## Installation
+
+Install my-project with bash:
+
+```bash
+  >mkdir my-rep
+  >git clone https://github.com/d1ejest/green-ap1.git my-rep
+  >python3 my-rep/main.py
+```
+Open http://127.0.0.1:5000
+in your browser to check if it works.
+## Requirements
+- python ~= 3.11
+- flask ~= 3.0.0
--- main.py
+++ main.py
@@ -1,43 +1,97 @@
-from flask import Flask, render_template, request, jsonify
-from whatsapp_api_client_python import API
+from flask import Flask, render_template, request
+import requests
+import json
 
 app = Flask(__name__)
 
-# Объявим idInstance и apiTokenInstance глобальными переменными
 
-USER = API.GreenApi(request.form['idInstance'], request.form['apiTokenInstance'])
-
-
-# Интерфейс веб-приложения на index.html
+# Интерфейс index.html
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Метод получения настроек
+
+# Получение ид и токена с от пользователя и отправка запроса на получение настроек
 @app.route('/settings', methods=['POST'])
 def settings():
-    response = USER.getSettings()
-    return jsonify({'answer': response.text.encode('utf8')})
+    data = request.json
+    idInstance = data.get('id')
+    apiTokenInstance = data.get('token')
 
-# Метод получения состояния
+    url = f'https://api.green-api.com/waInstance{idInstance}/getSettings/{apiTokenInstance}'
+
+    payload = {}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return response.text.encode('utf8')
+
+
+# Получение ид и токена с от пользователя и отправка запроса на получение состояния
 @app.route('/state', methods=['POST'])
 def state():
-    response = USER.getStateInstance()
-    return jsonify({'answer': response.text.encode('utf8')})
+    # Можно было убрать ид и токен в кортеж, но не было времени
+    data = request.json
+    idInstance = data.get('id')
+    apiTokenInstance = data.get('token')
 
-# Метод отправки сообщения
+    url = f'https://api.green-api.com/waInstance{idInstance}/getStateInstance/{apiTokenInstance}'
+
+    payload = {}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return response.text.encode('utf8')
+
+
+# Отправка сообщения на указанный номер
 @app.route('/sendtxt', methods=['POST'])
 def sendtxt():
-    response = USER.sending.sendMessage('chatId', request.form['message'])
-    return jsonify({'answer': response.text.encode('utf8')})
+    data = request.json
+    idInstance = data.get('id')
+    apiTokenInstance = data.get('token')
 
-# Метод отправки файлов
+    # Не знал откуда вытащить ид чата
+    chatId = data.get('chatId')
+    message = data.get('message')
+
+    url = f'https://api.green-api.com/waInstance{idInstance}/sendMessage/{apiTokenInstance}'
+
+    payload = json.dumps({
+        "chatId": chatId,
+        "message": message
+    })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return response.text.encode('utf8')
+
+
+# Отправка файла
 @app.route('/sendfile', methods=['POST'])
-def sendimage():
-    url = request.form['url']
-    filename = url.split('/')[-1]
-    response = USER.sending.sendFileByUrl('chatId',url,filename)
-    return jsonify({'answer': response.text.encode('utf8')})
+def sendfile():
+    data = request.json
+    idInstance = data.get('id')
+    apiTokenInstance = data.get('token')
+
+    # Не знал откуда вытащить ид чата снова
+    chatId = data.get('chatId')
+    file = data.get('file')
+
+    url = f'https://api.green-api.com/waInstance{idInstance}/sendFileByUrl/{apiTokenInstance}'
+
+    payload = json.dumps({
+        "chatId": chatId,
+        "file": file
+    })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return response.text.encode('utf8')
 
 
 if __name__ == '__main__':
